dfaf29c23832dbbbc8ef1dc305461488c8a7aee8
EliG-123 <eliganz@outlook.com>
2022-12-18 19:46:27 -0800

    /server

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const WorkboxWebpackPlugin = require("workbox-webpack-plugin");
 const isProduction = process.env.NODE_ENV == "production";
 
 const config = {
-  entry: "server.js",
+  entry: "/server.js",
   output: {
     path: path.resolve(__dirname, "dist"),
   },
